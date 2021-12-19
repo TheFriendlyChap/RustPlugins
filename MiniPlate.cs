4313bc788cc17ff931cccc905eedbb689336e9e0
The Friendly Chap <93049415+TheFriendlyChap@users.noreply.github.com>
2021-12-19 18:12:16 +0200

    Added Plugin Code

---
+++ MiniPlate.cs
@@ -0,0 +1,138 @@
+﻿using Newtonsoft.Json;
+using System.Linq;
+using UnityEngine;
+
+namespace Oxide.Plugins
+{
+    [Info("Minicopter Licence Plate", "The Friendly Chap", "1.0.6")]
+    [Description("Spawn a licence plate (Small Wooden Board) at the back of the minicopter, with optional permissions.")]
+    class MiniPlate : RustPlugin
+    {
+        const string _platePrefab = "assets/prefabs/deployable/signs/sign.small.wood.prefab";
+        PlateManager _manager = new PlateManager();
+        private static readonly Vector3 PlatePosition = new Vector3(0.0f, 0.20f, -0.85f);
+        private static readonly Quaternion PlateRotation = Quaternion.Euler(180, 0, 180);
+
+        private ConfigData configData;
+        class ConfigData
+        {
+            [JsonProperty(PropertyName = "Use Permissions")]
+            public bool miniPerms = false;
+        }
+        #region ConfigFileStuff
+        private bool LoadConfigVariables()
+        {
+            try
+            {
+                configData = Config.ReadObject<ConfigData>();
+            }
+            catch
+            {
+                return false;
+            }
+            SaveConfig(configData);
+            return true;
+        }
+
+        void Init()
+        {
+            permission.RegisterPermission("miniplate.use", this);
+            FindBabies();
+
+            if (!LoadConfigVariables())
+            {
+                Puts("Config file issue detected. Please delete file, or check syntax and fix.");
+            }
+        }
+
+        void FindBabies()
+        {
+            //			Puts("FindBabies Running");
+            foreach (var playerMini in BaseNetworkable.serverEntities.OfType<MiniCopter>())
+            {
+                if (playerMini.HasChild(playerMini))
+                {
+                    //					Puts("Child Found");
+                    foreach (var babyEnt in playerMini.children)
+                    {
+                        if (babyEnt.PrefabName == _platePrefab)
+                        {
+                            //							Puts("Got Prefab, destroying MeshCollider");
+                            foreach (var mesh in babyEnt.GetComponentsInChildren<MeshCollider>())
+                            {
+                                UnityEngine.Object.DestroyImmediate(mesh);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        protected override void LoadDefaultConfig()
+        {
+            Puts("Creating new config file.");
+            configData = new ConfigData();
+            SaveConfig(configData);
+        }
+        void SaveConfig(ConfigData config)
+        {
+            Config.WriteObject(config, true);
+        }
+        #endregion
+        public string userID = "0";
+        void OnEntitySpawned(MiniCopter mini, ConfigData config)
+        {
+            if (configData.miniPerms)
+            {
+                userID = $"{mini.OwnerID}";
+                Puts($"Perms Active, mini userID {userID}");
+                if (!permission.UserHasPermission(userID, "miniplate.use"))
+                {
+                    return;
+                }
+                else
+                {
+                    _manager.Setup(mini);
+                }
+                return;
+            }
+            else
+            {
+                _manager.Setup(mini);
+                return;
+            }
+        }
+
+        class PlateManager
+        {
+            public void Setup(BaseVehicle vehicle)
+            {
+                MakeNumplate(vehicle, PlatePosition);
+            }
+
+            void MakeNumplate(BaseVehicle vehicle, Vector3 position)
+            {
+                BaseEntity entity = GameManager.server.CreateEntity(_platePrefab, vehicle.transform.position, PlateRotation);
+                if (entity == null) return;
+                entity.SetParent(vehicle);
+                entity.transform.localPosition = position;
+                entity.Spawn();
+                DestroyMeshCollider(entity);
+            }
+
+            void DestroyMeshCollider(BaseEntity ent)
+            {
+                //Makes it basically only visable since everything passes though it.
+                foreach (var mesh in ent.GetComponentsInChildren<MeshCollider>())
+                {
+                    UnityEngine.Object.DestroyImmediate(mesh);
+                    //No break if not grounded
+                    UnityEngine.Object.DestroyImmediate(ent.GetComponent<DestroyOnGroundMissing>());
+                    //No break if hits ground
+                    UnityEngine.Object.DestroyImmediate(ent.GetComponent<GroundWatch>());
+                    //Stops Decay
+                    UnityEngine.Object.DestroyImmediate(ent.GetComponent<DeployableDecay>());
+                }
+            }
+        }
+    }
+}
---
+++ XmasMini.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Newtonsoft.Json;
+using System.Linq;
+using System;
+
+namespace Oxide.Plugins
+{
+    [Info("Xmas Mini", "The Friendly Chap", "1.0.3")]
+    [Description("Spawns christmas lights on the minicopter. Merry X-Mas.")]
+    class XmasMini : RustPlugin
+    {
+        #region Defines
+        const string prefabName = "assets/prefabs/misc/xmas/christmas_lights/xmas.lightstring.deployed.prefab";
+        private static readonly Vector3 prefabPosition = new Vector3(0.08f, 0.21f, 0.6f);
+        private static readonly Quaternion prefabRotation = Quaternion.Euler(180, 90, 180);
+        private static readonly Vector3 prefabPosition2 = new Vector3(0.0f, 0.65f, -1.2f);
+        private static readonly Quaternion prefabRotation2 = Quaternion.Euler(180, 90, 178);
+        #endregion
+
+        #region Hooks
+        void OnEntitySpawned(MiniCopter mini) => Setup(mini);
+        #endregion
+
+        #region Functions
+        public void Setup(MiniCopter mini)
+        {
+            SpawnLights(mini, prefabPosition, prefabRotation);
+            SpawnLights(mini, prefabPosition2, prefabRotation2);
+        }
+
+        void SpawnLights(MiniCopter mini, Vector3 position, Quaternion rotation)
+        {
+            BaseEntity entity = GameManager.server.CreateEntity(prefabName, mini.transform.position, rotation);
+            if (entity == null) return;
+            entity.SetParent(mini);
+            entity.transform.localPosition = position;
+            entity.Spawn();
+        }
+        #endregion
+    }
+}
